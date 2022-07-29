f5e670dfe60c1d652547e669169dd0a7616d8c4f
marianettimatias <marianettimatiass@gmail.com>
2022-07-29 20:24:27 -0300

    29-7 CSS Form 75%

--- client/src/components/CreateDog/CreateDog.jsx
+++ client/src/components/CreateDog/CreateDog.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { postDogs, getTemperaments } from "../../actions";
 import { useDispatch, useSelector } from "react-redux";
+import './CreateDog.css'
 
 
 function validate(input) {
@@ -30,7 +31,7 @@ function validate(input) {
     if (!/^[0-9]*$/.test(input.life_span_max)) {
         error.life_span_max = 'La edad debe ser un valor numerico mayor a 0';
     }
-    
+
     return error;
 }
 
@@ -123,64 +124,66 @@ export default function PostDog() {
     return (
         <div>
             <h1>Crea una nueva Raza!</h1>
-            <form onSubmit={(e) => { handleSubmit(e) }} >
-                <div>
-                    <label >Nombre: </label>
+            <form className="form" onSubmit={(e) => { handleSubmit(e) }} >
+                <div className="form-all">
+                    <label className="input">Nombre: </label>
                     <input type="text"
                         placeholder="Nombre de la raza "
                         name='name'
                         value={input.name}
                         onChange={handleInput}
                     />
-                    {input.name === '' ? <p>*</p> : ''}
+                    {input.name === '' ? <p className="ast">*</p> : ''}
                 </div>
-                <div>
-                    <label >Altura mínima: </label>
+                <div className="form-all">
+                    <label className="input" >Altura mínima: </label>
                     <input type="text"
                         name='height_min'
                         placeholder="centímetros"
                         value={input.height_min}
                         onChange={handleInput}
                     />
-                    {input.height_min === '' ? <p>*</p> : ''}
+                    {input.height_min === '' ? <p className="ast">*</p> : ''}
                     {error.height_min && <p>{error.height_min}</p>}
                 </div>
-                <div>
-                    <label>Altura máxima: </label>
+                <div className="form-all">
+                    <label className="input">Altura máxima: </label>
                     <input type="text"
                         name='height_max'
                         placeholder="centímetros"
                         value={input.height_max}
                         onChange={handleInput}
                     />
-                    {input.height_max === '' ? <p>*</p> : ''}
+                    {input.height_max === '' ? <p className="ast">*</p> : ''}
                     {error.height_max && <p>{error.height_max}</p>}
                 </div>
-                <div>
-                    <label >Peso mínimo: </label>
+                <div className="form-all">
+                    <label className="input" >Peso mínimo: </label>
                     <input type="text"
                         placeholder="Kgs "
                         name='weight_min'
                         value={input.weight_min}
                         onChange={handleInput}
                     />
-                    {input.weight_min === '' ? <p>*</p> : ''}
+                    {input.weight_min === '' ? <p className="ast">*</p> : ''}
                     {error.weight_min && <p>{error.weight_min}</p>}
                 </div>
-                <div>
-                    <label >Peso máximo: </label>
+                <div className="form-all">
+                    <label className="input" >Peso máximo: </label>
                     <input type="text"
                         placeholder="Kgs "
                         name='weight_max'
                         value={input.weight_max}
                         onChange={handleInput}
                     />
-                    {input.weight_max === '' ? <p>*</p> : ''}
+                    {input.weight_max === '' ? <p className="ast">*</p> : ''}
                     {error.weight_max && <p>{error.weight_max}</p>}
                 </div>
-                <div>
-                    <label >Imagen: </label>
-                    <input type="text"
+                <div className="form-image">
+                    <label className="input-name" >Imagen: </label>
+                    <input
+                        className="input-image"
+                        type="text"
                         placeholder="Url de la imagen "
                         name='image'
                         value={input.image}
@@ -189,57 +192,66 @@ export default function PostDog() {
 
                     {error.image && <p>{error.image}</p>}
                 </div>
-                <div>
-                    <label >Años de vida (desde): </label>
+                <div className="form-all">
+                    <label className="input-life-name">Años de vida (desde): </label>
                     <input type="text"
                         placeholder="Años "
                         name='life_span_min'
                         value={input.life_span_min}
                         onChange={handleInput}
                     />
-                    {input.life_span_min === '' ? <p>*</p> : ''}
+                    {input.life_span_min === '' ? <p className="ast">*</p> : ''}
                     {error.life_span_min && <p>{error.life_span_min}</p>}
                 </div>
-                <div>
-                    <label >Años de vida (hasta): </label>
+                <div className="form-all">
+                    <label className="input-life-name">Años de vida (hasta): </label>
                     <input type="text"
                         placeholder="Años"
                         name='life_span_max'
                         value={input.life_span_max}
                         onChange={handleInput}
                     />
-                    {input.life_span_max === '' ? <p>*</p> : ''}
+                    {input.life_span_max === '' ? <p className="ast">*</p> : ''}
                     {error.life_span_max && <p>{error.life_span_max}</p>}
                 </div>
-                <div>
-                    {input.temperaments === '' ? <p>*</p> : ''}
-                    <span>Temperamentos: </span>
-                    <select onChange={(e) => { handleSelect(e) }}  >
+                <div >
+                    <div className="form-temps">
+                        {input.temperaments === '' ? <p className="ast">*</p> : ''}
+                        <span className="temps-name" >Temperamentos: </span>
+                        <select onChange={(e) => { handleSelect(e) }}  >
 
-                        {
-                            alltemperaments.map(temp => {
-                                return <option key={temp.id} value={temp.name}> {temp.name} </option>
+                            {
+                                alltemperaments.map(temp => {
+                                    return <option key={temp.id} value={temp.name}> {temp.name} </option>
 
-                            })
-                        }
-                    </select>
-                    <ul>
-                        <span>Temperamentos seleccionados: </span>
+                                })
+
+                            }
+
+                        </select>
+                        <p className="ast">*</p>
+                    </div>
+                    <div className="temps-list">
+                        <ul>
+                            <span>Temperamentos seleccionados: </span>
+                            {
+                                input.temperaments.map((e) => {
+                                    return <li value={e} key={e}> {e} <button value={e} onClick={handleDelete} >X</button>  </li>
+                                })
+                            }
+                        </ul>
+                        <p className="ast">* Campos obligatorios</p>
+                    </div>
+                    <div>
                         {
-                            input.temperaments.map((e) => {
-                                return <li value={e} key={e}> {e} <button value={e} onClick={handleDelete} >X</button>  </li>
-                            })
+                            input.name === '' || input.height_min === '' || input.height_max === '' || input.weight_min === '' || input.weight_max === '' || input.name === '' || input.life_span_min === '' ||
+                                input.life_span_max === '' || input.temperaments.length === 0 ?
+                                <button type="submit" disabled={true} >Faltan datos por completar</button> :
+                                <button type="submit">Crear Raza</button>
                         }
-                    </ul>
-                    <p>* Campos obligatorios</p>
-                    {
-                        input.name===''||input.height_min===''||input.height_max===''||input.weight_min===''||input.weight_max===''||input.name===''||input.life_span_min===''||
-                        input.life_span_max===''||input.temperaments.length===0?
-                        <button type="submit" disabled={true} >Faltan datos por completar</button>:
-                        <button type="submit">Crear Raza</button>
-                    }
-                    
-                    
+
+                    </div>
+
 
                 </div>
 
